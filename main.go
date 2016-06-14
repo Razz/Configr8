48b15d8d4f3c1548c9b58dd778faa649d50e08e6
Alex Cornford <amc42294@gmail.com>
2016-06-14 14:52:20 -0400

    Re worked mapping. Now any nessary variables can be added into DataMapFull, and mapped to a template. This is how env vars are being loaded. Should add more flexability. Could see database use as a plug in in the future.

--- main.go
+++ main.go
@@ -7,7 +7,34 @@ import (
 	"io/ioutil"
 	"os"
 	"strings"
+
+	"github.com/davecgh/go-spew/spew"
 )
+
+//Maps Variables for use in template. IE. Env : {EnvVar: EnvKey}
+
+//creates a Slice of DataMaps that will be consolidated into one big map
+//ths will it's self be returned into a datamap that looks like:
+//"Data" : { DataMap[GlobalVar] : {ScopedVar: Scoped Value}}
+//Ie. "Data": {
+//       "env":
+//         {"user": "jsmith",
+//          "shell": "ZSH",
+//         }
+//        }
+type DataMap map[string]map[string]string
+type DataMapSlice []DataMap
+type DataMapFull map[string]map[string]string
+
+func (dm DataMapSlice) consolidate() DataMapFull {
+	endDM := make(DataMapFull)
+	for _, i := range dm {
+		for k, v := range i {
+			endDM[k] = v
+		}
+	}
+	return endDM
+}
 
 func times(n int) []int {
 	var sliceInt []int
@@ -21,41 +48,48 @@ func debug() string {
 	return "Test"
 }
 
-func map_env() map[string]string {
-	env_vars := make(map[string]string)
+func MapEnv() map[string]string {
+	dm := make(map[string]string)
 	for _, env := range os.Environ() {
 		sep := strings.Index(env, "=")
-		env_vars[env[0:sep]] = env[sep+1:]
+		dm[env[0:sep]] = env[sep+1:]
 	}
-	return env_vars
+	return dm
+}
+
+func add(x int, y int) int {
+	return x + y
 }
 
 func main() {
 	var (
 		tmplLoc string
+		// dest         string
+		dataMapSlice DataMapSlice
 	)
 
-	flag.String(tmplLoc, "tmpl", "Location of Template to be parsed")
-	// tmplLoc := "/tmp/exmaple.tmpl"
+	flag.StringVar(&tmplLoc, "tmpl", "tmpl", "Location of Template to be parsed?")
+	// flag.StringVar(&dest, "dest", "dest", "Where is the parsed template going? Default: stdout")
+	flag.Parse()
+
 	tmplSrc, err := ioutil.ReadFile(tmplLoc)
+
+	dataMapSlice = append(dataMapSlice, DataMap{"env": MapEnv()})
 
 	tmpl := template.New("t").Funcs(template.FuncMap{
 		"times": times,
 		"debug": debug,
-		"env":   map_env,
+		"add":   add,
 	})
 
 	t, err := tmpl.Parse(string(tmplSrc))
 
-	if err != nil {
-		panic(err)
-	}
+	dataMaps := dataMapSlice.consolidate()
+	spew.Dump(dataMaps)
 
-	err = t.Execute(os.Stdout, map_env())
+	err = t.Execute(os.Stdout, dataMaps)
 
 	if err != nil {
 		fmt.Errorf("%s", err)
 	}
-
-	return
 }
